abb37745dfc9cb78c5174724778b107fe57cf6b2
Mikhail Udalov <m.udalov@cloudberry.bi>
2023-11-20 21:46:11 +0300

    Создание системы комментариев

--- blog/models.py
+++ blog/models.py
@@ -30,7 +30,7 @@ class Post(models.Model):
                               choices=Status.choices,
                               default=Status.DRAFT)
     
-    objects = models.Manager() # менеджерб применяемый по умолчанию
+    objects = models.Manager() # менеджер, применяемый по умолчанию
     published = PublishedManager() # конкретно-прикладной менеджер
 
     class Meta:
@@ -48,3 +48,24 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
